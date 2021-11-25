953cca7ada21036b5c2aece9406805efcc704a58
murmilad <murmilad@gmail.com>
2021-11-26 01:44:22 +0300

    Formic for book

--- react/src/components/Books/CreateBookForm.js
+++ react/src/components/Books/CreateBookForm.js
@@ -2,52 +2,69 @@ import {useState} from "react";
 import {createBook} from "../../actions/book-actions";
 import {useDispatch} from "react-redux";
 import {isStringEmpty, isObjectEmpty} from "../../utils/utils"
+import {Formik, Field, Form} from "formik"
+import * as yup from "yup"
 
 function CreateBookForm() {
   const dispatch = useDispatch()
   const [bookName, setBookName] = useState("")
   const [bookAuthor, setBookAuthor] = useState("")
-  const [errors, setErrors] = useState({})
-  const onSubmit = async () => {
-    let errors = {}
-    if (isStringEmpty(bookName)) {
-      errors.bookName='required'
-    }
-    if (isStringEmpty(bookAuthor)) {
-      errors.bookAuthor='required'
-    }
-    if (isObjectEmpty(errors)) {
-      dispatch({type: 'CREATE_BOOK', book: {
-        title: bookName,
-        author: bookAuthor
-      }})
-      setBookName("")
-      setBookAuthor("")
-      setErrors({})
-    } else {
-      setErrors(errors)
-    }
-  }
+
   return (
-    <div className="create_book_form_wrapper">
-      <form className="create_book_form row" onSubmit={e=>{e.preventDefault(); onSubmit()}}>
-        <div className="create_book_input col-md-6" >
+    <Formik
+      initialValues = {{
+        bookName: '',
+        bookAuthor: '',
+      }}
+      onSubmit ={ values => {
+        dispatch({type: 'CREATE_BOOK', book: {
+          title: values.bookName,
+          author: values.bookAuthor
+        }})
+        setBookName("")
+        setBookAuthor("")
+      }}
+      validationSchema = {
+        yup.object().shape({
+          bookName: yup.string().required(),
+          bookAuthor: yup.string().required(),
+        })
+      }
+    >
+      <Form className="create_book_form row" >
+      <div className="create_book_form_wrapper">
+      <Field name="bookName">
+      {({
+      field,
+      form: { touched, errors },
+      meta,
+      }) => (
+          <div className="create_book_input col-md-6" >
           <label htmlFor="bookName" className="form-label">Book Title</label>
-          <input type="text" className="form-control" id="bookName" value={bookName}
-                 onChange={e=>setBookName(e.target.value)} />
-          {errors.bookName && <span className="form_error">This field is required</span>}
+          <input className="form-control" {...field} />
+          {meta.touched && meta.error && ( <span className="form_error">This field is required</span>)}
         </div>
-        <div className="create_book_input col-md-6">
+      )}
+      </Field>
+      <Field name="bookAuthor">
+      {({
+      field,
+      form: { touched, errors },
+      meta,
+      }) => (
+          <div className="create_book_input col-md-6">
           <label htmlFor="bookAuthor" className="form-label">Book Author</label>
-          <input type="text" className="form-control" id="bookAuthor" value={bookAuthor}
-                 onChange={e=>setBookAuthor(e.target.value)} />
-          {errors.bookAuthor && <span className="form_error">This field is required</span>}
+          <input className="form-control" {...field} />
+          {meta.touched && meta.error && ( <span className="form_error">This field is required</span>)}
         </div>
-        <div className="create_book_form_add_btn_wrapper">
+      )}
+      </Field>
+      <div className="create_book_form_add_btn_wrapper">
             <button type="submit" className="btn btn-primary">Create book</button>
         </div>
-      </form>
-    </div>
+        </div>
+      </Form>
+    </Formik>
   )
 }
 
